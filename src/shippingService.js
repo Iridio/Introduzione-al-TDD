40dfcb728e4462cf0a594fd544192953bd70d775
Alessandro Colla <alessandro.colla@evoluzionetelematica.it>
2017-05-03 08:52:44 +0200

    Tutti i test passano

--- src/shippingService.js
+++ src/shippingService.js
@@ -19,14 +19,105 @@ module.exports = {
     }
 
     //1. Looking for Courier
-    let courierParams = getCourier(params.courier);
+    let courierParams = getCourierInfo(params.courier);
     if (courierParams === undefined) {
       return { requestValid: false, shippingFees: 0, message: 'Corriere non identificato' };
     }
 
-    // Qui implementeremo i test
-
+    //2. Compute StandardShippingFees
+    let shippingFees = getStandardShippingFees(params.products, courierParams.weights, courierParams.volumes);
 
     return { requestValid: true, shippingFees: shippingFees, message: '' };
   }  
 };
+
+function getCourierInfo(courier) {
+  for (let i = 0; i < couriers.length; i++) {
+    if (couriers[i].courier === courier)
+      return couriers[i];
+  }
+
+  return undefined;
+}
+
+function getStandardShippingFees(products, weights, volumes) {
+  let shippingFees = 0;
+  let totalWeight = 0;
+  let totalVolumePerMeter = 0;
+
+  //#region Compute totalWeight && totalVolumePerMeter
+  for (let i = 0; i < products.length; i++) {
+    totalWeight += ( products[i].weight * products[i].quantity);
+    let volumePerMeter = (products[i].volume * products[i].quantity ) / 5000000;
+    totalVolumePerMeter += volumePerMeter;
+  }
+  //#endregion
+
+  //#region PrezzoPerVolume
+  let volumeFees = 0;
+  for (let v = 0; v < volumes.length; v++) {
+    if (totalVolumePerMeter >= volumes[v].min && volumes[v].max <= totalVolumePerMeter) {
+      volumeFees = volumes[v].price;
+    }
+  } 
+  //#endregion
+  
+  //# region PrezzoPerPeso
+  let weightFees = 0;
+  for (let p = 0; p < weights.length; p++) {
+    if (totalWeight >= weights[p].min && weights[p].max <= totalWeight) {
+      weightFees = weights[p].price;
+    }
+  }
+  //#endregion    
+
+  //#region Compare Fees
+  if (weightFees > volumeFees) {
+    shippingFees += weightFees;
+  } else {
+    shippingFees += volumeFees;
+  }
+  //#endregion
+
+  return shippingFees;
+}
+
+function chkFreeShipping(shippingFees, products) {
+  let totalValue = 0;
+
+  //#region Compute totalWeight && totalVolumePerMeter
+  for (let i = 0; i < products.length; i++) {
+    totalValue += (products[i].pricePerUnit * products[i].quantity);
+  }
+  //#endregion
+
+  //#region ChkSpedizioneGratuita
+  if (totalValue >= 100) {
+    shippingFees = 0;
+  }
+  //#endregion
+
+  return shippingFees;
+}
+
+function addServiceCost(shippingFees, courierService) {
+    if (courierService === 'Express') {
+        shippingFees += (shippingFees * 30 / 100);
+    }
+
+    return shippingFees;
+}
+
+// Possibile refactor: Aggiungere sconti/maggiorazioni sulla destinazione
+// in couriers.
+function addDestinationCost(shippingFees, courier, destination) {
+    if (courier === 'DHL' && destination === 'BRA') {
+        shippingFees -= (shippingFees * 2/100);
+    }
+
+    if (courier === 'UPS' && destination === 'ITA') {
+        shippingFees += 3;
+    }
+
+    return shippingFees;
+}
